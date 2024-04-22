13e78a37ec194972b387eed1701c84bfff8fcfd9
riku.yokoyama <riku.yokoyama@wealthnavi.com>
2024-04-22 15:48:04 +0900

    add storeFunction

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,12 +1,17 @@
 <script>
+import { useStoreUsers } from '@/stores/store'
 import axios from 'axios'
 import { reactive, onMounted } from 'vue'
 export default {
   setup() {
+    const storeUsers = useStoreUsers();
     const data = reactive({
       title: 'JSON Placeholderからのユーザー情報取得',
       users: []
     })
+    const deleteTasks = (id) => {
+      storeUsers.deleteUser(id)
+    }
     const getData = () => {
       let url = 'https://jsonplaceholder.typicode.com/users/'
       axios.get(url).then((result) => {
@@ -16,61 +21,65 @@ export default {
         data.users = result.data
       })
     }
+
     onMounted(() => {
       getData()
+      storeUsers(data.users);
     })
     return { data, getData }
   }
 }
+
+
 </script>
 
 
 <template>
   <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">id</th>
-      <th scope="col">name</th>
-      <th scope="col">email</th>
-      <th scope="col">address</th>
-      <th scope="col">phone</th>
-      <th scope="col">website</th>
-      <th scope="col">company</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <button>編集</button>
-      <button>削除</button>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <button>編集</button>
-      <button>削除</button>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <button>編集</button>
-      <button>削除</button>
-    </tr>
-  </tbody>
-</table>
+    <thead>
+      <tr>
+        <th scope="col">id</th>
+        <th scope="col">name</th>
+        <th scope="col">email</th>
+        <th scope="col">address</th>
+        <th scope="col">phone</th>
+        <th scope="col">website</th>
+        <th scope="col">company</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <td>@mdo</td>
+        <button>編集</button>
+        <button>削除</button>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <button>編集</button>
+        <button>削除</button>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <button>編集</button>
+        <button>削除</button>
+      </tr>
+    </tbody>
+  </table>
 </template>
