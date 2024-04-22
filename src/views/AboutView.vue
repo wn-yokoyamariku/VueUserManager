3b59c37d8a81729e1c8d30eb39d0a567fd2b658d
Fujiwara Masataka <masataka.fujiwara@wealthnavi.com>
2024-04-22 15:56:26 +0900

    追加画面

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,14 +2,45 @@
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">id</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">name</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">email</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">adress</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">phone</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">website</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  <div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1">company</span>
+    <input type="text" class="form-control" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+
+  <button type="button" class="btn">追加</button>
 </template>
 
 <style>
-@media (min-width: 1024px) {
   .about {
-    min-height: 100vh;
     display: flex;
     align-items: center;
   }
-}
+
 </style>
