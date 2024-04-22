bf9fbcea382a82e35045e66df18c00de30ac893c
yusuke.tsuchimoto <yusuke.tsuchimoto@wealthnavi.com>
2024-04-22 16:04:02 +0900

    header追加

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,10 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import TopHeader from './components/common/TopHeader.vue';
 </script>
 
 <template>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-
+  <TopHeader />
   <RouterView />
 </template>
 
